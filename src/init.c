684d3944474f441a51c9805477206ea016ec071c
Pierrick Naessens <pierrick.naessens62@gmail.com>
2025-01-20 17:29:29 +0100

    need function check cmd and exec cmd

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 13:11:51 by pnaessen          #+#    #+#             */
-/*   Updated: 2025/01/16 12:24:25 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2025/01/20 17:26:30 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	main(int argc, char **argv, char **env)
 {
 	int		pipe_fd[2];
 	pid_t	pid1;
+	pid_t	pid2;
 
 	// char *cmd = "cat";
 	// cmd = get_path(cmd, env);
@@ -24,7 +25,7 @@ int	main(int argc, char **argv, char **env)
 		ft_printf("Usage: ./pipex file1 cmd1 cmd2 file2\n");
 		return (EXIT_FAILURE);
 	}
- 	if (pipe(pipe_fd) == -1)
+	if (pipe(pipe_fd) == -1) // 0 pour la lecture 1 pour ecriture
 	{
 		perror("Pipe failed");
 		return (EXIT_FAILURE);
@@ -37,46 +38,56 @@ int	main(int argc, char **argv, char **env)
 	}
 	if (pid1 == 0)
 		handle_child(argv, pipe_fd, env);
-	handle_parent(argv, pipe_fd, env);
+	pid2 = fork();
+	if (pid2 < 0)
+	{
+		perror("Fork failed");
+		return (EXIT_FAILURE);
+	}
+	if (pid2 == 0)
+		handle_parent(argv, pipe_fd, env);
 	waitpid(pid1, NULL, 0);
 	return (EXIT_SUCCESS);
 }
 
 void	handle_child(char **argv, int pipe_fd[2], char **env)
 {
-	int		file1;
-	char	*cmd1[] = {"/bin/sh", "-c", argv[2], NULL};
+	int	file1;
 
+	close(pipe_fd[0]);
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 < 0)
 	{
 		perror("Error opening file1");
 		exit(EXIT_FAILURE);
 	}
-	if (dup2(file1, STDIN_FILENO) == -1)
-	{
-		perror("dup2 file1 -> STDIN failed");
-		exit(EXIT_FAILURE);
-	}
 	if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
 	{
+		close(pipe_fd[1]);
+		close(file1);
 		perror("dup2 pipe -> STDOUT failed");
 		exit(EXIT_FAILURE);
 	}
+	if (dup2(file1, STDIN_FILENO) == -1)
+	{
+		close(pipe_fd[1]);
+		close(file1);
+		perror("dup2 file1 -> STDIN failed");
+		exit(EXIT_FAILURE);
+	}
 	close(file1);
-	close(pipe_fd[0]);
 	close(pipe_fd[1]);
-	execve(cmd1[0], cmd1, env);
-	perror("Execve cmd1 failed");
-	exit(EXIT_FAILURE);
+	if (!check_cmd(argv[2]))
+		perror("Execve cmd1 failed");
+	exec_cmd(argv[2], env);
 }
 
 void	handle_parent(char **argv, int pipe_fd[2], char **env)
 {
-	int		file2;
-	char	*cmd2[] = {"/bin/sh", "-c", argv[3], NULL};
+	int	file2;
 
-	file2 = open(argv[4], O_WRONLY | O_CREAT);
+	close(pipe_fd[1]);
+	file2 = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (file2 < 0)
 	{
 		perror("Error opening file2");
@@ -84,19 +95,23 @@ void	handle_parent(char **argv, int pipe_fd[2], char **env)
 	}
 	if (dup2(pipe_fd[0], STDIN_FILENO) == -1)
 	{
+		close(file2);
+		close(pipe_fd[0]);
 		perror("dup2 pipe -> STDIN failed");
 		exit(EXIT_FAILURE);
 	}
 	if (dup2(file2, STDOUT_FILENO) == -1)
 	{
+		close(file2);
+		close(pipe_fd[0]);
 		perror("dup2 file2 -> STDOUT failed");
 		exit(EXIT_FAILURE);
 	}
 	close(file2);
 	close(pipe_fd[0]);
-	close(pipe_fd[1]);
-	execve(cmd2[0], cmd2, env);
-	perror("Execve cmd2 failed");
+	if (!check_cmd(argv[4]))
+		perror("Execve cmd2 failed");
+	exec_cmd(argv[4], env);
 	exit(EXIT_FAILURE);
 }
 
